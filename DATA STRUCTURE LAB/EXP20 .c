5b21f702dc41af6228b6d2ee6ddc2b936563600e
Sherin Mariya Robert <147114833+mariya-sherin@users.noreply.github.com>
2024-01-28 20:51:17 +0530

    Updated EXP20 .c

--- DATA STRUCTURE LAB/EXP20 .c
+++ DATA STRUCTURE LAB/EXP20 .c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 
 const int inf = 99999;
-int k, a, b, u, v, n, e = 1, mincost = 0, s[9] = {0}, G[50][50];
+int k, a, b, u, v, n, ne = 1, mincost = 0, parent[9] = {0}, adj[50][50], i = 0, j = 0;
 
 int find(int i) {
-    while (s[i] != 0)
-        i = s[i];
+    while (parent[i] != 0)
+        i = parent[i];
     return i;
 }
 
-int combine(int i, int j) {
+int union(int i, int j) {
     if (i != j) {
-        s[j] = i;
+        parent[j] = i;
         return 1;
     }
     return 0;
 }
 
 void kruskals() {
-    while (e < n) {
+    while (ne < n) {
         int min = inf;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (G[i][j] < min) {
-                    min = G[i][j];
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if (adj[i][j] < min) {
+                    min = adj[i][j];
                     a = u = i;
                     b = v = j;
                 }
@@ -32,12 +32,12 @@ void kruskals() {
         }
         u = find(u);
         v = find(v);
-        if (combine(u, v) != 0) {
+        if (union(u, v) != 0) {
             printf("Edge between vertex %d and vertex %d has cost %d\n", a, b, min);
             mincost += min;
         }
-        G[a][b] = G[b][a] = 99999;
-        e++;
+        adj[a][b] = adj[b][a] = 99999;
+        ne++;
     }
     printf("Minimum cost = %d\n", mincost);
 }
@@ -46,11 +46,11 @@ int main() {
     printf("Enter the number of vertices : ");
     scanf("%d", &n);
     printf("Enter the cost matrix: ");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &G[i][j]);
-            if (G[i][j] == 0) {
-                G[i][j] = inf;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+            if (adj[i][j] == 0) {
+                adj[i][j] = inf;
             }
         }
     }
